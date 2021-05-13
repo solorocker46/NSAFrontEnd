000260513ce52ddf200cac1dfb6d895243dd039e
solorocker46 <danahurt4@gmail.com>
2021-05-13 16:36:18 +0530

    Components update

--- TrialInstitute/src/app/app.module.ts
+++ TrialInstitute/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddInstituteComponent } from './InstitutionComponents/add-institute/add-institute.component';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { AddMinistryComponent } from './MinistryComponent/add-ministry/add-ministry.component';
+import { AddOfficerComponent } from './OfficerComponent/add-officer/add-officer.component';
+import { AddStudentComponent } from './StudentComponent/add-student/add-student.component';
+import { AddScholarshipComponent } from './ScholarshipComponent/add-scholarship/add-scholarship.component';
+import { LoginComponent } from './UserComponent/login/login.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddInstituteComponent
+    AddInstituteComponent,
+    AddMinistryComponent,
+    AddOfficerComponent,
+    AddStudentComponent,
+    AddScholarshipComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
